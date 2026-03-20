0ccf7d3f240cd25fc5f7a9ce734103f7871e5dea
Lueur3 <sgdm515@gmail.com>
2026-03-20 20:58:45 +0300

    some corrects

--- src/utils.py
+++ src/utils.py
@@ -9,11 +9,11 @@ PUNCTUATION_MAP = str.maketrans("", "", string.punctuation)
 def validate_file_path(filename: str) -> str:
     if filename:
         file_path = os.path.abspath(filename.rstrip())
-        if not (os.path.exists(file_path)):
+        if not os.path.exists(file_path):
             raise FileNotFoundError(f"The file at {file_path} was not found.")
-        elif not (os.path.splitext(file_path)[1] in SUPPORTED_EXTENSIONS):
+        elif not os.path.splitext(file_path)[1] in SUPPORTED_EXTENSIONS:
             raise ValueError("The file contains an incorrect extension type.")
-        elif not (os.access(file_path, os.R_OK)):
+        elif not os.access(file_path, os.R_OK):
             raise PermissionError(f"The file does not have access rights.")
 
         return file_path
