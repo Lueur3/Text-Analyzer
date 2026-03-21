7c428d965f41597c7e10808ba2f1f423bc44cc7b
Lueur3 <sgdm515@gmail.com>
2026-03-21 22:01:01 +0300

    processor update dict tests

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,36 +1,6 @@
 import pytest
 from src.processor import init_stats, update_stats, get_top_words
 
-"""
-@pytest.mark.parametrize(
-    "type, filename, expected_dict",
-    [
-        (
-            1,
-            "../data/example_text.txt",
-            {
-                "statistic_type": "File - Text-Analyzer/data/example_text.txt",
-                "total_count": 0,
-                "word_frequencies": {},
-                "longest_word": "",
-            },
-        ),
-        (
-            2,
-            "User text",
-            {
-                "statistic_type": "User text",
-                "total_count": 0,
-                "word_frequencies": {},
-                "longest_word": "",
-            },
-        ),
-    ],
-)
-def test_init_stats(type, filename, expected_dict):
-    assert init_stats(type, filename) == expected_dict
-"""
-
 
 @pytest.mark.parametrize(
     "text_type, filename, expected_dict_type",
@@ -73,3 +43,61 @@ def test_keys_dict(text_type, filename, expected_keys):
 )
 def test_values_dict(text_type, filename, expected_dict):
     assert init_stats(text_type, filename) == expected_dict
+
+
+@pytest.fixture
+def example_dict():
+    dc = init_stats("User text")
+    return dc
+
+
+@pytest.mark.parametrize(
+    "line, expected_res",
+    [
+        (
+            "apple banana apple",
+            {"apple": 2, "banana": 1},
+        )
+    ],
+)
+def test_update_stats(line, expected_res, example_dict):
+    update_stats(line, example_dict)
+    assert example_dict["word_frequencies"] == expected_res
+
+
+@pytest.mark.parametrize(
+    "line, expected_res", [("apple banana ananas cherry", "cherry")]
+)
+def test_long_word_stats(line, expected_res, example_dict):
+    update_stats(line, example_dict)
+    assert example_dict["longest_word"] == expected_res
+
+
+def test_empty_update(example_dict):
+    data = example_dict
+    update_stats("", example_dict)
+    assert example_dict == data
+
+
+@pytest.mark.parametrize(
+    "line, expected_res",
+    [
+        (
+            "banana apple banana cherry banana apple ananas",
+            {"banana": 3, "apple": 2, "cherry": 1, "ananas": 1},
+        )
+    ],
+)
+def test_duplicate_words(line, expected_res, example_dict):
+    update_stats(line, example_dict)
+    assert example_dict["word_frequencies"] == expected_res
+
+
+@pytest.mark.parametrize(
+    "line, expected_res",
+    [("banana apple cherry cherry", {"banana": 2, "apple": 2, "cherry": 4})],
+)
+def test_sum_effects(line, expected_res, example_dict):
+    update_stats(line, example_dict)
+    update_stats(line, example_dict)
+    assert example_dict["word_frequencies"] == expected_res
